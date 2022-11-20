d8295dd5acaea5896c288fba015f007817cdc955
legioner26 <ademar26@mail.ru>
2022-11-20 19:21:04 +0300

    20.1-20.2

--- 20.2.go
+++ 20.2.go
@@ -5,7 +5,7 @@ import "fmt"
 func multiplicationMatrix(matrix1 [][]int, matrix2 [][]int) [][]int {
 	result := make([][]int, len(matrix1))
 	for i := 0; i < len(matrix1); i++ {
-		result[i] = make([]int, len(matrix2))
+		result[i] = make([]int, len(matrix2[i]))
 		for j := 0; j < len(matrix2[i]); j++ {
 			for k := 0; k < len(matrix2); k++ {
 				result[i][j] += matrix1[i][k] * matrix2[k][j]
