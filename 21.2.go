d46eee2c750d47b31072bbfa4b146e301f4f6853
legioner26 <ademar26@mail.ru>
2022-11-24 17:31:00 +0300

    21.1-21.2

--- 21.2.go
+++ 21.2.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 )
 
-func matFunc(x int, y int, A func(int, int) int) int {
+func matfunc(x int, y int, A func(int, int) int) int {
 	defer fmt.Println("Результат:")
 	return A(x, y)
 }
 
 func main() {
-	one := matFunc(23, 3, func(a int, b int) int { return a - b })
+	one := matfunc(23, 3, func(a int, b int) int { return a - b })
 	fmt.Printf("первая функция вернула - %d \n", one)
 
-	two := matFunc(12, 6, func(a int, b int) int { return a + b })
+	two := matfunc(12, 6, func(a int, b int) int { return a + b })
 	fmt.Printf("вторая функция вернула - %d \n", two)
 
-	three := matFunc(3, 7, func(a int, b int) int { return a * b })
+	three := matfunc(3, 7, func(a int, b int) int { return a * b })
 	fmt.Printf("третья функция вернула - %d \n", three)
 
 }
